f43ff0475ee34ccd513a10837839569ceea291d6
Vi <kinjeks@gmail.com>
2021-06-23 11:56:21 +0300

    fix burger menu and section review

--- src/scripts/overlay.js
+++ src/scripts/overlay.js
@@ -1,10 +1,22 @@
+
+//Получение элементов
 const burgerBtn = document.querySelector('.header-tablets__menu');
 const overlay = document.querySelector('.overlay');
 const body = document.body;
+const menuMain = document.querySelector('.menu__list-mobile > .menu__item');
 
-
+//Добавление/удаление overlay по кнопке burger
 burgerBtn.addEventListener('click',() => {
   overlay.classList.toggle('overlay-active');
   burgerBtn.classList.toggle('header__tablets__menu-active');
-  body.classList.toggle("body-active")
-})
+  body.classList.toggle("body-active");
+});
+
+
+
+//Удаление overlay по кнопке "Главная"
+menuMain.addEventListener('click',()=>{
+  overlay.classList.remove('overlay-active')
+  burgerBtn.classList.remove('header__tablets__menu-active');
+  body.classList.remove("body-active");
+});
