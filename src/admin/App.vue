959d261de501b356b6bc4ef5a0d2edc864782da0
ViRybalkin <kinjeks@gmail.com>
2021-07-01 15:30:13 +0300

    add components skill and category

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -8,7 +8,27 @@
      <div class="container-content">
         <div class="header">
           <div class="title">Блок "Обо мне"</div>
-           <iconed-button type="iconed" @click="onClick" title="Добавить группу" />
+           <iconed-button 
+           type="iconed"
+           v-if="emptyCatisShow === false"
+            @click="emptyCatisShow = true" 
+            title="Добавить группу" />
+        </div>
+        <div class="skills">
+          <li class="item" v-if="emptyCatisShow">
+            <category 
+            @remove="emptyCatisShow = false"
+            empty 
+            />
+          </li>
+          <li class="item"
+            v-for="category in categories"
+            :key="category.id">
+            <category 
+             :title="category.category"
+            :skills="category.skills"
+            />
+          </li>
         </div>
      </div>
     </div>
@@ -20,12 +40,23 @@ import user from './components/user/user'
 import headline from './components/headline/headline'
 import navigation from './components/navigation/navigation'
 import button from './components/button'
+import category from "./components/categoty/category";
 export default{
   components:{
     headline,
     user,
     navigation,
-    iconedButton:button
+    iconedButton:button,
+    category
+    },
+    data(){
+      return{
+        categories:[],
+        emptyCatisShow:false
+      }
+    },
+    created(){
+      this.categories = require('./data/categories.json')
     }
   }
 </script>
