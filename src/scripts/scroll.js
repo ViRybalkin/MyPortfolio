384bb49953b6379ddc20d42dd7d0f5b9bf9b9a2a
ViRybalkin <kinjeks@gmail.com>
2021-08-11 13:36:12 +0300

    fix bugs in scrolling

--- src/scripts/scroll.js
+++ src/scripts/scroll.js
@@ -1,3 +1,18 @@
+// const anchors = document.querySelectorAll(".menu__link");
+
+// anchors.forEach((el) => {
+//   el.addEventListener("click", (e) => {
+//     e.preventDefault();
+
+//     const blockID = el.getAttribute("href");
+
+//     document.querySelector(blockID).scrollIntoView({
+//       behavior: "smooth",
+//       block: "center",
+//     });
+//   });
+// });
+
 const anchors = document.querySelectorAll(".menu__link");
 
 anchors.forEach((el) => {
@@ -6,9 +21,12 @@ anchors.forEach((el) => {
 
     const blockID = el.getAttribute("href");
 
-    document.querySelector(blockID).scrollIntoView({
+    const block = document.querySelector(blockID).getBoundingClientRect();
+
+    window.scrollTo({
+      left: block.left + window.pageYOffset,
+      top: block.top + window.pageXOffset,
       behavior: "smooth",
-      block: "center",
     });
   });
 });
